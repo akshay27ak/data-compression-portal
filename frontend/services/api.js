07267fa46f1454e6039f55cf0ccf8fd4dc181327
akshay27ak <akshay27k47@gmail.com>
2025-06-23 22:20:21 +0530

    url fixedd

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000" || "https://data-compression-portal-tawny.vercel.app"
 
 // Upload file
 export const uploadFile = async (file) => {
